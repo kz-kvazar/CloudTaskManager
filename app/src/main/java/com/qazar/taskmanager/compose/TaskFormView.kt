02a98e3d705026ac300d5055f49517c798c79c1c
kz-kvazar <milniy87@gmail.com>
2023-11-03 00:06:01 +0200

    date piker added

--- app/src/main/java/com/qazar/taskmanager/compose/TaskFormView.kt
+++ app/src/main/java/com/qazar/taskmanager/compose/TaskFormView.kt
@@ -1,77 +1,64 @@
 package com.qazar.taskmanager.compose
 
-import android.widget.DatePicker
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.qazar.taskmanager.Task
-import androidx.compose.foundation.layout.*
 import java.util.*
 
 class TaskFormView {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Preview(showBackground = true)
     @Composable
     fun TaskForm(
-        onTaskCreated: (Task) -> Unit
+//        onTaskCreated: (Task) -> Unit
     ) {
         var title by remember { mutableStateOf("") }
         var description by remember { mutableStateOf("") }
         var selectedDate by remember { mutableStateOf<Date?>(null) }
 
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().background(Color.Green)) {
             Row(modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Title")
+                Text(text = "Task title:", modifier = Modifier.fillMaxWidth(0.2f))
             }
             BasicTextField(
-                value = title,
-                onValueChange = { newTitle ->
+                value = title, onValueChange = { newTitle ->
                     title = newTitle
-                },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
+                }, singleLine = true, modifier = Modifier.fillMaxWidth()
             )
 
             Row(modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Description")
+                Text(text = "Task description", modifier = Modifier.fillMaxWidth(0.2f))
             }
             BasicTextField(
-                value = description,
-                onValueChange = { newDescription ->
+                value = description, onValueChange = { newDescription ->
                     description = newDescription
-                },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
+                }, singleLine = true, modifier = Modifier.fillMaxWidth()
             )
 
             Row(modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Due Date")
+                Text(text = "Expiration date", modifier = Modifier.fillMaxWidth(0.2f))
             }
+            val state = rememberDatePickerState(initialDisplayMode = DisplayMode.Companion.Input)
             DatePicker(
-                selectedDate = selectedDate,
-                onDateSelected = { date ->
-                    selectedDate = date
-                },
-                modifier = Modifier.fillMaxWidth()
+                state = state,
             )
 
-            Button(
-                onClick = {
-                    val newTask = Task(
-                        title = title,
-                        description = description,
-                        time = selectedDate?.time
-                    )
-                    onTaskCreated(newTask)
-                    title = ""
-                    description = ""
-                    selectedDate = null
-                }
-            ) {
+            Button(onClick = {
+                val newTask = Task(
+                    title = title, description = description, time = selectedDate?.time
+                )
+                // onTaskCreated(newTask)
+                //title = ""
+                //description = ""
+                //selectedDate = null
+            }) {
                 Text(text = "Create Task")
             }
         }
